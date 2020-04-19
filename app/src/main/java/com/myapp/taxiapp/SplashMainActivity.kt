33ce3bbf6a9f60438357f99b255331265f78ed32
Venue 11 Pro <robin839168gmail.com>
2020-04-19 15:58:04 +0300

    initialization commit

---
+++ app/src/main/java/com/myapp/taxiapp/SplashMainActivity.kt
@@ -0,0 +1,11 @@
+package com.myapp.taxiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity_main)
+    }
+}
